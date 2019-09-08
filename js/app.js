ea92f9d71603a33d16b31bec8bad6087ef70d3f1
Mostafa Yasin <mostafa.yassin@lavaloon.com>
2019-09-08 13:48:39 +0200

    Finished first example of todo-list using vue.js

--- js/app.js
+++ js/app.js
@@ -3,24 +3,30 @@ $(function () {
 
     Vue.component('todo-item', {
         props: ['todo'],
-        template: '<div class="mb-2">\
-        <button class="text-danger btn">\
-            <i class="fas fa-times"></i>\
-        </button>\
-        <button class="btn-success btn" v-if="todo.done">\
-            <i class="fas fa-check"></i>\
-        </button>\
-        <button class="btn-light btn text-success" v-else>\
-            <i class="fas fa-check"></i>\
-        </button>\
-        {{ todo.text }}\
-    </div>'
+        template: '\
+            <div class="mb-2">\
+                <button v-on:click="$emit(\'remove\')" class="text-danger btn">\
+                    <i class="fas fa-times"></i>\
+                </button>\
+                <button class="btn-success btn" v-if="todo.done" v-on:click="$emit(\'uncomplete\')">\
+                    <i class="fas fa-check"></i>\
+                </button>\
+                <button class="btn-light btn text-success" v-on:click="$emit(\'complete\')" v-else>\
+                    <i class="fas fa-check"></i>\
+                </button>\
+                {{ todo.text }}\
+            </div>'
     });
 
     var app = new Vue({
         el: "#app",
         data: {
-            welcome_message: "Vue.js",
+            first_name: "Mostafa",
+            last_name: "Yasin",
+            title: "TO-DO Examples",
+            classObj: {
+                'text-dark': true,
+            },
             new_item_demo: "",
             date: "you loaded this page at " + new Date().toLocaleTimeString(),
             seen: true,
@@ -55,12 +61,47 @@ $(function () {
             clear_input: function () {
                 $("#new_item_value").val("");
                 this.new_item_demo = "";
+            },
+            complete_all: function () {
+                this.todos.forEach(todo => {
+                    todo.done = true
+                });
+            },
+            uncomplete_all: function () {
+                this.todos.forEach(todo => {
+                    todo.done = false
+                });
+            },
+            delete_completed: function () {
+                // this.todos.forEach( (todo, i) => {
+                //     todo.done ? this.todos.splice(this.todos.indexOf, 1) : ''
+                // });
+                this.todos = this.todos.filter(function (todo) {
+                    return !todo.done
+                });
             }
         },
         computed: {
+            full_name: {
+              // getter
+              get: function () {
+                return this.first_name + ' ' + this.last_name
+              },
+              // setter
+              set: function (newValue) {
+                var names = newValue.split(' ')
+                this.firstName = names[0]
+                this.lastName = names[names.length - 1]
+              }
+            },
             time: function () {
                 return new Date().toLocaleTimeString();
-            }   
-        }
+            }
+          },
+          watch: {
+            todos: function () {
+                console.log("Watched.");
+            },
+          }
     })
 });
